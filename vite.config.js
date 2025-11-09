ac72d4ac3e5e45fcd4595350739cd83efbbd46de
BagaProg <bakenb09@gmail.com>
2025-11-10 01:14:41 +0500

    Add PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss(),
+  VitePWA({
+    registerType: "autoUpdate",
+    manifest: {
+      name: "EcoCityHub",
+      short_name: "EcoCityHub",
+      description: "Smart ecological solutions for cities",
+      theme_color: "#4CAF50",
+      background_color: "#ffffff",
+      display: "standalone",
+      start_url: "/",
+      icons: [
+        { src: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
+        { src: "/icons/icon-512.png", sizes: "512x512", type: "image/png" }
+      ]
+    }
+  })
+  ],
   proxy: {
     '/api': 'http://localhost:5000'
   }
