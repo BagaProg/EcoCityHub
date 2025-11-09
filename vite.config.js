7d4f18fc26e31748bb899cf95d316dae9bc17719
BagaProg <bakenb09@gmail.com>
2025-11-10 00:23:50 +0500

    Fix Vite config for Render deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/EcoCityHub/",
+  base: "./",
   plugins: [react(), tailwindcss(),],
   proxy: {
     '/api': 'http://localhost:5000'
